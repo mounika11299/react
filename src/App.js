11cd22d0d40aef7bd593fdc097a93bb670f36aac
mounika11299 <96199431+mounika11299@users.noreply.github.com>
2022-09-24 09:49:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ChangeColors from "./ChangeColors";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
+export default App;
